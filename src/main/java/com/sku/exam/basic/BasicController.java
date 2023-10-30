48f377bab6a821b3794052fd874faf6b59d1c469
hiwoogi <rmcprpwjd001@gmail.com>
2023-10-30 09:38:39 +0900

    commit

--- src/main/java/com/sku/exam/basic/BasicController.java
+++ src/main/java/com/sku/exam/basic/BasicController.java
@@ -43,19 +43,18 @@ public class BasicController {
 
 
         String requestBody = "{\"startDate\":\"2017-08-01\"," +
-                "\"endDate\":\"2017-09-30\"," +
+                "\"endDate\":\"2018-09-30\"," +
                 "\"timeUnit\":\"month\"," +
-                "\"category\":[{\"name\":\"패션의류\",\"param\":[\"50000000\"]}," +
-                "{\"name\":\"화장품/미용\",\"param\":[\"50000002\"]}]," +
-                "\"device\":\"pc\"," +
-                "\"ages\":[\"20\",\"30\"]," +
-                "\"gender\":\"f\"}";
+                "\"category\":\"50000000\"," +
+                "\"keyword\": [{\"name\":\"패션의류\",\"param\":[\"정장\"]}]," +
+                "\"device\":\"\"," +
+                "\"ages\":[]," +
+                "\"gender\":\"\"}";
 
         String responseBody = post(dataApiURL, requestHeaders, requestBody);
 
 
         return responseBody;
-
 
     }
 
