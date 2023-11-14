40c06b677a6073e7024eaa4c4402a0e5ceca6f92
hiwoogi <rmcprpwjd001@gmail.com>
2023-11-14 13:52:55 +0900

    실시간 검색어 스크래핑 성공

---
+++ src/main/java/com/sku/exam/basic/controller/ScrapingController.java
@@ -0,0 +1,34 @@
+package com.sku.exam.basic.controller;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/test2")
+public class ScrapingController {
+
+    @GetMapping("/scrape/{cid}")
+    public ResponseEntity<String> scrapeWebsite(@PathVariable String cid) {
+        String url = "https://datalab.naver.com/shoppingInsight/getKeywordRank.naver?timeUnit=date&cid=" + cid;
+
+        try {
+            Document document = Jsoup.connect(url)
+                    .userAgent("Your User Agent")  // Replace with your user agent
+                    .referrer("https://datalab.naver.com/")
+                    .get();
+
+            String scrapedData = document.text();
+            return ResponseEntity.ok(scrapedData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Failed to scrape the website.");
+        }
+    }
+}
